b2275b29ba753fa20ec15881169b0425ba0ea5e6
keshitrivedi <keshitrivedi@gmail.com>
2025-03-31 14:41:47 +0530

    backgroundhehe

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 require 'src/Dependencies'
 
+local background_scroll = 0
+
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
@@ -14,6 +16,7 @@ function love.load()
     })
 
     gTextures = {
+        ['sky'] = love.graphics.newImage('graphics/skybck8.png'),
         ['main'] = love.graphics.newImage('graphics/pikminsprites2.png'),
         ['bigflowers'] = love.graphics.newImage('graphics/flowers.png')
     }
@@ -52,6 +55,7 @@ function love.keyboard.wasPressed(key)
 end
 
 function love.update(dt)
+    background_scroll = (background_scroll + BACKGROUND_SCROLL_SPEED*dt) % BACKROUND_LOOPING_POINT
     gStateMachine:update(dt)
 
     love.keyboard.keysPressed = {}
@@ -61,6 +65,7 @@ function love.draw()
     love.graphics.clear(173/255, 216/255, 230/255, 1)
     push:apply('start')
 
+    love.graphics.draw(gTextures['sky'], -background_scroll, 0)
     gStateMachine:render()
 
     push:apply('end')
